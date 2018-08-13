968ca1e93457fbd6df92187925cc0861ff6981aa
Remo Mueller <remosm@gmail.com>
2018-08-13 17:19:03 -0400

    Version bump to 1.0.0.beta11

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module LoftHF
     MAJOR = 1
     MINOR = 0
     TINY = 0
-    BUILD = "beta10" # "pre", "beta1", "beta2", "rc", "rc2", nil
+    BUILD = "beta11" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
