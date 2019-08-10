a5165facbb37f40034d93960ede9646b7b3ff8a6
Remo Mueller <remosm@gmail.com>
2019-08-10 13:55:58 -0400

    Report rows can be marked as emphasized to group rows together visually, closes #9.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_155952) do
+ActiveRecord::Schema.define(version: 2019_08_10_172859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 2019_08_10_155952) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "muted", default: false, null: false
+    t.boolean "emphasized", default: false, null: false
     t.index ["position"], name: "index_report_rows_on_position"
     t.index ["report_id"], name: "index_report_rows_on_report_id"
   end
